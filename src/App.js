4e1562769d66dbfa16ce7260ef3b3d2c71b1ce01
talal <talal_kasem@hotmail.com>
2019-05-18 17:51:33 -0400

    project cleaned

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import SearchForm from "./Components/SearchForm";
+import { Carousel } from "react-bootstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      imgs: []
+    };
+  }
+
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  performSearch = (query = "jaguar") => {
+    const API_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+    axios
+      .get(
+        `https://api.unsplash.com/search/photos/?page=1&per_page=10&query=${query}&client_id=${API_KEY}`
+      )
+      .then(data => {
+        this.setState({ imgs: data.data.results });
+      })
+      .catch(err => {
+        console.log("Fetching error", err);
+      });
+  };
+
+  render() {
+    const searchResults = this.state.imgs;
+
+    return (
+      <div>
+        <div className="titleSearchContainer">
+          <p className="title">Unspalsh Carousel</p>
+          <SearchForm onSearch={this.performSearch} />
+        </div>
+        <div>
+          <Carousel className="carouselMain">
+            {searchResults.map(image => {
+              return (
+                <Carousel.Item key={image.id} className="carouselItem">
+                  <img
+                    src={image.urls.full}
+                    className="d-block"
+                    alt={image.id}
+                  />
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
